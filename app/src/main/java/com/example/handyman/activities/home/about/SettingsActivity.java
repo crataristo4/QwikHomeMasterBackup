a6ca3d0cd3ca316e480abf43f459deaef38ec55b
Botnet <crataristo4@gmail.com>
2020-03-28 18:24:40 -0800

    Update from crash

--- app/src/main/java/com/example/handyman/activities/home/about/SettingsActivity.java
+++ app/src/main/java/com/example/handyman/activities/home/about/SettingsActivity.java
@@ -3,23 +3,21 @@ package com.example.handyman.activities.home.about;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.handyman.R;
+import com.example.handyman.databinding.SettingsActivityBinding;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    SettingsActivityBinding settingsActivityBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        settingsActivityBinding = DataBindingUtil.setContentView(this, R.layout.settings_activity);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+
     }
 
     @Override
